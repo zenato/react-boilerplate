1e107cd9f24bae9d06374a5b618f77e7fdc1595d
Yeongjin Lee <yeongjinnn@gmail.com>
2017-05-09 19:44:41 +0900

    Fixed eslint error.

--- src/shared/components/Pagination.jsx
+++ src/shared/components/Pagination.jsx
@@ -9,20 +9,23 @@ type Props = {
   onSelect?: (page: number) => void,
 };
 
-const Pagination = ({ pages, page, onSelect }: Props) => !pages && !page
-  ? null
-  : (
-    <RBPagination
-      items={pages}
-      activePage={page}
-      onSelect={onSelect}
-      style={{ marginTop: 0 }}
-      first
-      last
-      prev
-      next
-    />
-  );
+const Pagination = ({ pages, page, onSelect }: Props) => {
+  const isNotExistPage = !pages && !page;
+  return isNotExistPage
+    ? null
+    : (
+      <RBPagination
+        items={pages}
+        activePage={page}
+        onSelect={onSelect}
+        style={{ marginTop: 0 }}
+        first
+        last
+        prev
+        next
+      />
+    );
+};
 
 Pagination.defaultProps = {
   pages: null,
